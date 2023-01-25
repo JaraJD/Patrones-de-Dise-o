0cd862dc962dc612215d3ed0eb8484117f80132f
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-01-24 22:16:49 -0500

    Patron Singleton
    Se implementa ejemplo del patron Singleton

---
+++ Singleton/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+	internal class Logger
+	{
+		private List<String> msgLog = new List<string>();
+		private static Logger instance = null;
+		protected Logger() { }
+
+		public static Logger Instance
+		{
+			get 
+			{
+				if (instance == null) { instance = new Logger(); }
+
+				return instance;
+			}
+		}
+
+		public void Log(string message)
+		{
+			msgLog.Add(message);
+		}
+
+		public void PrintLog()
+		{
+			foreach (var log in msgLog)
+			{
+				Console.WriteLine(log);
+			}
+		}
+	}
+}
